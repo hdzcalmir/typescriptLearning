ad66315eba8d9a145e8b65807cae84cbd44ea964
hdzcalmir <hdzcalmir@example.com>
2023-02-02 09:50:02 +0100

    done

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -10,6 +10,7 @@ type productType = {
 }
 
 export default async function updateOutput(id: string) {
+  // await response when async func
   const products = await getProducts();
   const output = document.querySelector(`#${id}`);
   const html = layoutProducts(products);
@@ -19,13 +20,12 @@ export default async function updateOutput(id: string) {
   }
 }
 
-
 function layoutProducts(products: productType[]) {
-  const items = products.map( (p)  => {
+  const items = products.map( ({id, icon, name})  => { // destructing parameters / objects
     const productHtml = `
-    <span class="card-id">#${p.id}</span>
-      <i class="card-icon ${p.icon} fa-lg"></i>
-    <span class="card-name">${p.name}</span>
+    <span class="card-id">#${id}</span>
+      <i class="card-icon ${icon} fa-lg"></i>
+    <span class="card-name">${name}</span>
     `;
     const cardHtml = `
     <li>
@@ -52,95 +52,163 @@ async function getProducts(): Promise<productType[]> {
   return products;
 }
 
-// // run our samples
-// runTheLearningSamples();
+// run our samples
+runTheLearningSamples();
 
-// function runTheLearningSamples() {
-//   // hoisted
-//   function displayProductInfo(id: number, name: string) {
-//     console.log(`${prefix} typed parameters`);
-//     console.log(`product id=${id} and name=${name}`)
-//   }
+function runTheLearningSamples() {
+  // hoisted
+  function displayProductInfo(id: number, name: string) {
+    console.log(`${prefix} typed parameters`);
+    console.log(`product id=${id} and name=${name}`)
+  }
 
-//   displayProductInfo(10, 'Pizza');
-//   // tip podatka koji saljemo kao argument u funkciju mora se podudarati sa tipom podatka koji funkcija prima
-//   // odnosno tipom podatka parametra
-//   // displayProductInfo(10, ['Pizza', 1, 'John'])
+  displayProductInfo(10, 'Pizza');
+  // tip podatka koji saljemo kao argument u funkciju mora se podudarati sa tipom podatka koji funkcija prima
+  // odnosno tipom podatka parametra
+  // displayProductInfo(10, ['Pizza', 1, 'John'])
 
-//   // function declaration su hoisted, sto znaci da su pri kompajlanju prebacene na vrh
-//   // koda, stranice, sto znaci da neovisno gdje je funkcija top tipa deklarisana moze
-//   // se pozvati bilo gdje
+  // function declaration su hoisted, sto znaci da su pri kompajlanju prebacene na vrh
+  // koda, stranice, sto znaci da neovisno gdje je funkcija top tipa deklarisana moze
+  // se pozvati bilo gdje
 
-//   // hoisted mean lifted to the top
+  // hoisted mean lifted to the top
 
-//   console.log(`${prefix} function declaration`);
-//   console.log(addNumbersDeclaration(10,15));
+  console.log(`${prefix} function declaration`);
+  console.log(addNumbersDeclaration(10,15));
   
-//   // : number na kraju znaci da ce return funkcije biti number / broj
-//   function addNumbersDeclaration(x: number, y: number) : number{
-//     const sum: number =  x + y;
-//     return sum;
-//   }
+  // : number na kraju znaci da ce return funkcije biti number / broj
+  function addNumbersDeclaration(x: number, y: number) : number{
+    const sum: number =  x + y;
+    return sum;
+  }
 
-//   // function expression is not hoisted, is not lifted to the top
-//   const addNumberExpression = function(x: number, y: number) {
-//     const sum: number = x + y;
-//     return sum;
-//   }
+  // function expression is not hoisted, is not lifted to the top
+  const addNumberExpression = function(x: number, y: number) {
+    const sum: number = x + y;
+    return sum;
+  }
 
-//   console.log(`${prefix} function expression`);
-//   console.log(addNumberExpression(100,15));
+  console.log(`${prefix} function expression`);
+  console.log(addNumberExpression(100,15));
 
-//   const sampleProducts = [
-//     {
-//       id: 10,
-//       name: 'Pizza Slice',
-//       icon: 'fas fa-pizza-slice',
-//     },
-//     {
-//       id: 20,
-//       name: 'Ice Cream',
-//       icon: 'fas fa-ice-cream',
-//     },
-//     {
-//       id: 30,
-//       name: 'Cheese',
-//       icon: 'fas fa-cheese',
-//     },
-//   ];
+  const sampleProducts = [
+    {
+      id: 10,
+      name: 'Pizza Slice',
+      icon: 'fas fa-pizza-slice',
+    },
+    {
+      id: 20,
+      name: 'Ice Cream',
+      icon: 'fas fa-ice-cream',
+    },
+    {
+      id: 30,
+      name: 'Cheese',
+      icon: 'fas fa-cheese',
+    },
+  ];
 
-//   // return ce biti string array
-//   function getProductName() : string[] {
-//     return sampleProducts.map((p) => p.name);
-//   }
+  // return ce biti string array
+  function getProductName() : string[] {
+    return sampleProducts.map((p) => p.name);
+  }
 
-//   console.log(`${prefix} return array`);
-//   // vraca nam array proizvoda, odnosno imena, a to je ako ne deklarisemo da 
-//   // output funkcije drugacijeg tipa podatka
-//   console.log(getProductName());
+  console.log(`${prefix} return array`);
+  // vraca nam array proizvoda, odnosno imena, a to je ako ne deklarisemo da 
+  // output funkcije drugacijeg tipa podatka
+  console.log(getProductName());
   
-//   // bitno da je da stavimo i undefined
-//   function getProductById(id: number) : productType | undefined {
-//     return sampleProducts.find(p => id === p.id)
-//   }
+  // bitno da je da stavimo i undefined
+  function getProductById(id: number) : productType | undefined {
+    return sampleProducts.find(p => id === p.id)
+  }
 
-//   console.log(`${prefix} return ProductType`);
+  console.log(`${prefix} return ProductType`);
 
-//   console.log(getProductById(20));
+  console.log(getProductById(20));
 
-//   // return type na ovoj funkciji je void
-//   function displayProducts(products: productType[]) : void {
-//     const productName = products.map(p => {
-//       const name = p.name.toLowerCase();
-//       return name;
-//     });
+  // return type na ovoj funkciji je void
+  function displayProducts(products: productType[]) : void {
+    const productName = products.map(p => {
+      const name = p.name.toLowerCase();
+      return name;
+    });
 
-//     const message = `Sample products include: ${productName.join(', ')}`;
-//     console.log(`${prefix} return void`);
-//     console.log(message);
+    const message = `Sample products include: ${productName.join(', ')}`;
+    console.log(`${prefix} return void`);
+    console.log(message);
     
-//   }
+  }
 
-//   displayProducts(sampleProducts);
+  displayProducts(sampleProducts);
 
-// }
+
+  // reuse
+  const {floor, random} = Math;
+  // arrow funkciji smo dodijelili default parametar od 1000
+  const getRandomInt = (max: number = 50) => floor(random() * max);
+
+  // icon parameter je opcionalan, a to dobijamo tako sto dodamo upitnik na kraju
+  // sto automatski znaci da moze imati vrijednost undefined
+  function createProduct(name: string, icon?: string) : productType {
+    const id = getRandomInt(1000);
+    return {
+      id, name, icon
+    };
+  }
+
+  console.log(`${prefix} Optional parameters`);
+  let pineapple = createProduct('pineapple', 'pin-apple.jpg');
+  let mango = createProduct('mango');
+  console.log(pineapple, mango);
+  
+
+  function createProductWithDefaults(name: string, 
+    // default parametar za ikonu
+    icon: string = 'generic-fruit.jpg') : productType {
+    const id = getRandomInt();
+    return {
+      id, name, icon
+    };
+  }
+
+  console.log(`${prefix} Default parameters`);
+  let pineappleDefaults = createProductWithDefaults('pineapple', 'pin-apple.jpg');
+  let mangoDefaults = createProductWithDefaults('mango');
+  console.log(pineappleDefaults, mangoDefaults);
+
+  // rest argumenti su opcionalni, ukoliko ne postoje nece se funkcija poremetiti
+  function buildAdress(street: string, city: string, ...restOfAddress: string[]) {
+    // konzolujemo tabelu
+    console.table(restOfAddress);
+    
+    // join metoda
+    const adress = `${street} ${city} ${restOfAddress.join(' ')}`;
+    return adress;
+  }
+
+  const someAddress = buildAdress(
+    '1 lois lane', // street
+    'smallville',  // city
+    'apt 101',  // restOfAddress [0]
+    'area 51', // restOfAddress [1]
+    'mystery country' // restOfAddress [2]
+    );
+
+    console.log(someAddress);
+    
+
+
+    	function displayProduct({id, name}: productType) : void {
+        console.log(`${prefix} Destructuring parameters`);
+        console.log(`Product id = ${id} and name = ${name}`);
+      }
+
+      const prod = getProductById(10);
+      if(prod) {
+        displayProduct(prod);
+      }
+
+
+}
